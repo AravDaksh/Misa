ac78b49770dd59c75750a0faa60c410924dda996
Arav <aravdakshxe@gmail.com>
2025-11-08 23:57:52 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
 // ===== FIXED HEADER FOR RENDER COMPATIBILITY =====
 if (!isset($GLOBALS['update'])) {
     $input = file_get_contents("php://input");
