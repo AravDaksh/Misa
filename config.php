bc46a5ef267e5551892d37acb6f010d322a29c4a
Arav <aravdakshxe@gmail.com>
2025-11-08 16:34:02 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 
-if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '__-B3');
-if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'Was_B3');
-if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '5579729798');
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'CherryWaffle');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'cherryyywaffle');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '5032657924');
 
-if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
-if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'BravCc_Bot');
-if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7048214022:AAGS75gaumDRfGtOOXb4YphshInKf9KzArw');
-if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-4194518429');
-if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002021756101');
+if (!defined('BOT_NAME'))               define('BOT_NAME', '「 Misa ↯ 」 🇮🇳');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'Misa_ChkBot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '8087657424:AAFvhhpizu2IstBDEzHvWF7RLbKuHa9O39k');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1003185328579');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1003185328579');
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
-if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'dbmisa');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'dpg-d47i5qili9vc738odr70-a');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'misa');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', '0xvhQSp1aZUIl7vL7oHOnZwalf5FkyOt');
